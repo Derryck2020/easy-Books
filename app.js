1a8b1610b02c5f5af93696b5aea795a63449658d
Derrick <oseiagyemanb@gmail.com>
2024-01-20 21:18:16 +0000

    update of file

--- app.js
+++ app.js
@@ -1,1 +1,58 @@
-console.log('E-Commerce API');
+//dotenv import
+require('dotenv').config();
+require('express-async-errors');
+
+//express package
+const express = require('express');
+const app = express();
+
+//auxillary packages
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+
+// database import
+const connectDB = require('./db/connect');
+
+// routers
+const authRouter = require('./routes/authRoutes');
+// const userRouter = require('./routes/userRoutes');
+// const productRouter = require('./routes/productRoutes');
+// const reviewRouter = require('./routes/reviewRoutes');
+// const orderRouter = require('./routes/orderRoutes');
+
+// middleware import
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(morgan('tiny'));
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+
+app.use(express.static('./public'));
+app.use(fileUpload());
+
+app.get('/', (req, res) => {
+	res.send('e-commerce api development');
+});
+
+app.use('/api/v1/auth', authRouter);
+// app.use('/api/v1/users', userRouter);
+// app.use('/api/v1/products', productRouter);
+// app.use('/api/v1/reviews', reviewRouter);
+// app.use('/api/v1/orders', orderRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URL);
+		app.listen(port, console.log(`Server is listening on port ${port}...`));
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
