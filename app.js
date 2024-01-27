6b9957969ad576cf8bab6c54b394c7a2ed311df5
Derrick <oseiagyemanb@gmail.com>
2024-01-27 21:09:22 +0000

    update of file

--- app.js
+++ app.js
@@ -16,10 +16,10 @@ const connectDB = require('./db/connect');
 
 // routers
 const authRouter = require('./routes/authRoutes');
-// const userRouter = require('./routes/userRoutes');
-// const productRouter = require('./routes/productRoutes');
-// const reviewRouter = require('./routes/reviewRoutes');
-// const orderRouter = require('./routes/orderRoutes');
+const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const orderRouter = require('./routes/orderRoutes');
 
 // middleware import
 const notFoundMiddleware = require('./middleware/not-found');
@@ -37,10 +37,10 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/v1/auth', authRouter);
-// app.use('/api/v1/users', userRouter);
-// app.use('/api/v1/products', productRouter);
-// app.use('/api/v1/reviews', reviewRouter);
-// app.use('/api/v1/orders', orderRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
+app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/orders', orderRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
