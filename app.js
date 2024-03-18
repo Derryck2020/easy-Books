a0eac3672299850b4d388e12f7ded181969d8763
Derrick <oseiagyemanb@gmail.com>
2024-03-18 11:05:29 +0000

    update of files

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const app = express();
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimiter = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 // database import
 const connectDB = require('./db/connect');
@@ -24,6 +29,19 @@ const orderRouter = require('./routes/orderRoutes');
 // middleware import
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.set('trust proxy', 1);
+app.use(
+	rateLimiter({
+		windowMs: 15 * 60 * 1000,
+		max: 60,
+	})
+);
+
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use(morgan('tiny'));
 app.use(express.json());
